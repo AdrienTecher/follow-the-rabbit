ab596456007aeafc26be5f9572d48df69e246eec
Alice in Cryptoland <8215948-aliceincryptoland@users.noreply.gitlab.com>
2021-10-05 03:15:11 +0200

    Changed the heading

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -30,8 +30,8 @@ export default function Home(): JSX.Element {
   const { siteConfig } = useDocusaurusContext();
   return (
     <Layout
-      title={`Hello from ${siteConfig.title}`}
-      description="Description will go into a meta tag in <head />"
+      title={`Welcome`}
+      description="A small journey into programming for the young professional"
     >
       <HomepageHeader />
       <main>
